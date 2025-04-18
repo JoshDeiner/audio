51129de579bfb1f25830070a3901afbff8849588
Josh Deiner <joshdeiner75@gmail.com>
2025-04-18 14:13:07 -0400

    test commit

--- test2.py
+++ test2.py
@@ -1,5 +1,5 @@
 def another_test():
-    print("This has trailing whitespace")    
+    print("This has trailing whitespace")
     x = 10
     if x > 5:
-        print("x is greater than 5")    
+        print("x is greater than 5")
