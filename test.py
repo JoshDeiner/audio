d9d884f6ecf07c241d460d4dcf747dcce062ff04
Josh Deiner <joshdeiner75@gmail.com>
2025-04-18 14:18:32 -0400

    remove test files

--- test.py
+++
@@ -1,5 +0,0 @@
-def test_function():
-    print("This is a test function with some linting issues")
-    x = 5
-    if x == 5:
-        print("x is 5")
--- test2.py
+++
@@ -1,5 +0,0 @@
-def another_test():
-    print("This has trailing whitespace")
-    x = 10
-    if x > 5:
-        print("x is greater than 5")
